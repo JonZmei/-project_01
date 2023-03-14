9a9b4f6b6a878ec354618588d6111adae9832227
Jon Zmeevskii <125925372+JonZmei@users.noreply.github.com>
2023-03-14 17:52:57 +0200

    Загрузка файла домашней работы 1.4

---
+++ dz 1,4 Список товаров .py
@@ -0,0 +1,32 @@
+
+titles = {
+    'Кроссовки тип 3 (Adidas)': '100000110',
+    'Мячик тип 2 (Adidas)': '100000146',
+    'Кепка тип 1 (Adidas)': '100000149',
+    'Ремень тип 2 (Nike)': '100000194',
+    'Футболка тип 1 (Adidas)': '100000224',
+    'Шапка тип 5 (Puma)': '100000280',
+}
+
+# Товары находятся на складе и сохранены в виде словаря списков словарей,
+# которые отражают количество товаров в магазине по каждому коду.
+
+store = {
+    '100000110': [{'quantity': 31, 'price': 1637}],
+    '100000146': [ {'quantity': 4, 'price': 45}, {'quantity': 10, 'price': 48}],
+    '100000149': [ {'quantity': 28, 'price': 279}, {'quantity': 32, 'price': 291}],
+    '100000194': [{'quantity': 8, 'price': 220}, {'quantity': 1, 'price': 170}],
+    '100000224': [{'quantity': 61, 'price': 438}, {'quantity': 23, 'price': 302},  {'quantity': 50, 'price': 412}],
+    '100000280': [{'quantity': 26, 'price': 175}, ]
+}
+
+total_value = {}
+for code, items in store.items():
+    total_value[code] = sum(item['quantity'] * item['price'] for item in items)
+
+# Выводим суммарную стоимость каждого товара в магазине
+for title, code in titles.items():
+    if code in store:
+        total_quantity = sum(item['quantity'] for item in store[code])
+        total_price = total_value[code]
+        print(f"{title} - {total_quantity} шт, стоимость {total_price} руб")
