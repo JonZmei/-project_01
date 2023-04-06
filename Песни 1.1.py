3253b008c98faa55a645741d86c1976475d02e4e
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-06 17:44:08 +0300

    Выполнено

--- Песни 1.1.py
+++ Песни 1.1.py
@@ -17,3 +17,32 @@ print(my_favorite_songs[21:38])
 
 # второй с конца трек
 print(my_favorite_songs[-22:-5])
+
+# Да, но можно считать не вручную конечно)
+# Решение с помощью индекции строк
+
+first_song = my_favorite_songs [
+    : my_favorite_songs.find(',')
+]
+
+last_song = my_favorite_songs [
+    my_favorite_songs.rfind('N') :
+]
+
+second_song = my_favorite_songs[
+     my_favorite_songs.find('S') : 
+     my_favorite_songs.find(', A')
+]
+
+previous_song = my_favorite_songs [
+    my_favorite_songs.rfind('St') : 
+    my_favorite_songs.rfind(', N')
+    ]
+
+print(first_song, last_song, second_song, previous_song)
+
+
+# Решение с помощью метода split() и индексации списков
+songs = my_favorite_songs.split(', ')
+
+print(songs[0], songs[-1], songs[1], songs[-2])
